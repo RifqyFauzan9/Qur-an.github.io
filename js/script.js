134853dfc8f1388093b1ef1273a03a119dfb75e6
Rifqy Fauzan <r1fqyf4uz4n@gmail.com>
2023-10-19 08:35:44 +0700

    second commit

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ function getSurat(){
     cardSurat+=`
     <div class="col-lg-3 col-md-4 col-sm-12">
     <div class="card mb-3 card-surat">
-    <div class="card-body">
+    <div class="card-body" onclick="location.href='surat.html?nomorsurat=${surat.nomor}'">
     <h5 class="card-title">${surat.nomor}. ${surat.nama_latin}</h5>
     <h3 class="card-subtitle mb-2 text-muted text-end">${surat.nama}</h3>
     <p class="card-text text-end">${surat.arti}</p>
