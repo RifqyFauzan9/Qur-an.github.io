e7bc389c0b371ad0da6593385cc9866a4b4979f3
Rifqy Fauzan <r1fqyf4uz4n@gmail.com>
2023-10-20 11:03:50 +0700

    second commit

--- js/surat.js
+++ js/surat.js
@@ -10,4 +10,26 @@ function getURL(e){
     }
 }
 const nomorsurat = getURL('nomorsurat');
-console.log(nomorsurat);
+// console.log(nomorsurat);
+
+function getSurat(){
+    fetch(`https://equran.id/api/surat/${nomorsurat}`)
+    .then(Response => Response.json())
+    .then(Response => {
+        const judulSurat = document.querySelector('.judul-surat');
+        const cardJudulSurat = `
+        <strong>${Response.nama_latin} <br>${Response.nama}</strong>
+                      <p>Jumlah Ayat : ${Response.jumlah_ayat} <br>Arti : ${Response.arti}</p>
+                      <button class="btn btn-success">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-circle-fill" viewBox="0 0 16 16">
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z"/>
+                          </svg>
+                        Play
+                    </button>
+                    `;
+                    judulSurat.innerHTML=cardJudulSurat
+
+        console.log(Response);
+    });
+}
+getSurat()
